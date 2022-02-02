a4be8ad372e12c9508362c339801f2b692fb24a1
KinzCode <47777487+KinzCode@users.noreply.github.com>
2022-02-02 17:15:19 +1100

    adjusted eda.py to have custom find order differencing

--- src/eda.py
+++ src/eda.py
@@ -17,34 +17,45 @@ def difference_series(df):
     fig, axes = plt.subplots(2, 2, sharex=True)
     axes[0, 0].plot(np.log(df['Price(USD)'])); axes[0, 0].set_title('Original Series')
     plot_acf(np.log(df['Price(USD)']), ax=axes[0, 1])
-    
     # 1st Differencing
     axes[1, 0].plot(np.log(df['Price(USD)']).diff()); axes[1, 0].set_title('1st Order Differencing')
     plot_acf(np.log(df['Price(USD)']).dropna(), ax=axes[1, 1])
-    
-
     return
 
 
-
-def find_order_of_differencing(df):
-    result = adfuller(df['Price(USD)'])
+def adf_test(target_series):
+    result = adfuller(target_series)
     adf_statistic = result[0]
     p_value = result[1]
     print('ADF Statistic: %f' % adf_statistic)
     print('p-value: %f' % p_value)
-    
+    return adf_statistic, p_value
+
+def find_order_of_differencing(df):
+    adf_statistic, p_value = adf_test(df['Price(USD)'])
     if p_value > 0.05:
-        difference_series(df)
-    return
+        print("P value to large, trying first differnce")
+        df['Price(USD)'] = np.log(df['Price(USD)'])
+        df['Price(USD)'] = df['Price(USD)'].diff()
+        df.dropna(inplace = True)
+        adf_statistic, p_value = adf_test(df['Price(USD)'])
+        if p_value < 0.05:
+            print("Success... P value achieved after first differencing")
+            return 1
+        elif p_value > 0.50:
+            print("Need to difference again")
+    else:
+        print("No difference Required")
+    return 
     
+def find_p(df):
+    return
 
 def eda(df):
     d = find_order_of_differencing(df)
-    return
+    p = find_p(df)
+    return d
     
-
-
 
 if __name__ == '__main__':
     # enter selected currency
